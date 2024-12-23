d5b9046b20b5b39d716bec9c3a184a750a3d6881
Sam Dillon <samuel.g.dillon@gmail.com>
2024-12-23 11:11:46 -0600

    fixing port

--- api/src/index.js
+++ api/src/index.js
@@ -14,6 +14,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '..', '..', 'ui', 'build', 'index.html'));
 });
 
-app.listen(9000, () => {
+app.listen(process.env.PORT || 9000, () => {
   console.log('Server started');
 });
